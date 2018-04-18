e2e7ae4f4fa3f2f24057adcbd72cb702c26eabe4
Steve V <steve@vanspall.id.au>
2018-04-18 14:24:11 +1000

    SV: Added documentation markup and generated yardoc

--- easy_options_parser.gemspec
+++ easy_options_parser.gemspec
@@ -9,6 +9,7 @@ Gem::Specification.new do |s|
   s.files       = ["lib/easy_options_parser.rb"]
   s.add_dependency "activesupport"
   s.add_development_dependency "rspec"
+  s.add_development_dependency "yard"
   s.add_development_dependency "byebug"
   s.license       = 'MIT'
 end
--- lib/easy_options_parser.rb
+++ lib/easy_options_parser.rb
@@ -7,28 +7,11 @@ require 'io/console'
 class EasyOptionsParser
   attr_reader :options, :cli_description
 
-  def infer_class(option_type)
-    return option_type if option_type.is_a?(Class)
-    return Array if option_type.to_s.match(/^array:?/)
-    return Array if option_type.to_s == 'dir_glob'
-    return String if %w[read_file dir].include?(option_type.to_s)
-
-    case option_type.to_s
-    when 'boolean'
-      BooleanParser
-    else
-      Class.const_get(option_type.to_s.classify)
-    end
-  end
-
-  def cli_description=(cli_description)
-    @cli_description = cli_description
-  end
-
-  def assign_to(assign_to)
-    @options = assign_to
-  end
-
+  # Creates an EasyOptionsParser with optional setup options
+  #
+  # @param [Hash] setup may contain one or all of the following
+  #   * :_cli_description - Is set it this option will override the default description in the help text (--help)
+  #   * :_assign_to - Not compltely tested. If set, the options passed to the command line will be set on the passed on object
   def initialize(setup = {})
     @main_path = caller_locations.detect{|l| l.base_label.match(/<main>/)}.try(:path) || 'main_file.rb'
     @defaults = {}
@@ -75,6 +58,24 @@ class EasyOptionsParser
     end
   end
 
+  # Overrides the description given in the help text. Calling this is the same as passing :_cli_description
+  # to the constructor
+  # @param [String] cli_description - The description to show in the help text
+  def cli_description=(cli_description)
+    @cli_description = cli_description
+  end
+
+  # Not fully tested. By default this the parse method will create an object whose methods will return the options passed in on the command line
+  # @param [Object] assign_to - the object to assign the commnd line option to. It is assumed that the appropriate writer methods
+  #                             available on the object
+  def assign_to(assign_to)
+    @options = assign_to
+  end
+
+  # Adds an option to the command line parser
+  # @param [Symbol] option_name - The name of the option. This will translate to an option flag. eg: :option_1 will define a cli option --option-1
+  # @param [Symbol] option_type - Determines the option type. Unless specified below this param will be evaluated to a class :string to String, :date_time to DateTime.
+  # @param [String] description - The description of this option. This will appear in the help text when a user passes --help
   def add_option(option_name, option_type, description = nil, additional_options = {})
     method_name = option_name.to_s.gsub(/\W/, '_')
 
@@ -118,19 +119,8 @@ class EasyOptionsParser
     self
   end
 
-  def process_individual_item_type(option_type, value)
-      if(option_type.to_s == 'dir_glob')
-        return Dir.glob(value[0])
-      elsif(option_type.to_s == 'dir')
-        raise "The path: #{value} does not exist" unless File.exist?(value)
-        raise "The path: #{value} is not a directory" unless File.directory?(value)
-      elsif(option_type.to_s == 'read_file')
-        raise "The path: #{value} does not exist" unless File.exist?(value)
-      end
-
-      return value
-  end
-
+  # Parses the command line arguments to the options specified in the setup
+  # @return [Object] Either the object passed into assign_to or a new object with the options defined avaiable as methods
   def parse
     @help_arg_passed = false
     @op.banner = "#{cli_description ? "#{cli_description}\n" : ""}Usage: ruby #{@main_path} [options]"
@@ -144,6 +134,8 @@ class EasyOptionsParser
 
     options
   end
+
+  private
 
   class ReadFile
     attr_reader :path
@@ -172,7 +164,32 @@ class EasyOptionsParser
     end
   end
 
-  private
+  def process_individual_item_type(option_type, value)
+    if(option_type.to_s == 'dir_glob')
+      return Dir.glob(value[0])
+    elsif(option_type.to_s == 'dir')
+      raise "The path: #{value} does not exist" unless File.exist?(value)
+      raise "The path: #{value} is not a directory" unless File.directory?(value)
+    elsif(option_type.to_s == 'read_file')
+      raise "The path: #{value} does not exist" unless File.exist?(value)
+    end
+
+    return value
+  end
+
+  def infer_class(option_type)
+    return option_type if option_type.is_a?(Class)
+    return Array if option_type.to_s.match(/^array:?/)
+    return Array if option_type.to_s == 'dir_glob'
+    return String if %w[read_file dir].include?(option_type.to_s)
+
+    case option_type.to_s
+    when 'boolean'
+      BooleanParser
+    else
+      Class.const_get(option_type.to_s.classify)
+    end
+  end
 
   def process_array_with_sub_type(option_type, value)
     sub_type = option_type.to_s.match(/^array:(\w+)/)[1]
